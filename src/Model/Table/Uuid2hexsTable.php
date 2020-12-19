294c7f8d15fe942345b56933ccb57b565cabd44c
Angel X. Moreno <angelxmoreno@gmail.com>
2020-12-19 06:37:40 -0500

    Added uniqueness to hex generator

--- src/Model/Table/Uuid2hexsTable.php
+++ src/Model/Table/Uuid2hexsTable.php
@@ -3,9 +3,9 @@ declare(strict_types=1);
 
 namespace App\Model\Table;
 
-use App\Model\Entity\Uuid2hex;
 use App\Model\TableBase;
 use Cake\Event\Event;
+use Cake\ORM\Entity;
 
 /**
  * Class Uuid2Hexs Table
@@ -15,13 +15,28 @@ class Uuid2HexsTable extends TableBase
 {
     /**
      * @param Event $event
-     * @param Uuid2hex $entity
+     * @param Entity $entity
      * @param \ArrayObject $options
      */
-    public function beforeSave(Event $event, Uuid2hex $entity, \ArrayObject $options)
+    public function beforeSave(Event $event, Entity $entity, \ArrayObject $options)
     {
         if (!$entity->hasValue('hex')) {
-            $entity->hex = substr(uniqid() . uniqid(), 0, 16);
+            $entity->set('hex', $this->generateUniqueHex());
         }
+    }
+
+    protected function generateUniqueHex(): string
+    {
+        $hex = $this->createRandom16CharHex();
+        if ($this->exists(compact('hex'))) {
+            return $this->createRandom16CharHex();
+        }
+
+        return $hex;
+    }
+
+    protected function createRandom16CharHex(): string
+    {
+        return substr(uniqid() . uniqid(), 0, 16);
     }
 }
