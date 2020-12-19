dc9dd914ec7c7632b0b4aa2a2015d67fec9be892
Angel X. Moreno <angelxmoreno@gmail.com>
2020-12-19 03:35:33 -0500

    removed unused dependencies

--- public/index.php
+++ public/index.php
@@ -11,71 +11,70 @@ use Slim\Factory\AppFactory;
 use Slim\Factory\ServerRequestCreatorFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
-
+try {
 //set up env
-$Loader = new Loader(__DIR__ . '/../.env');
+    $Loader = new Loader(__DIR__ . '/../.env');
 // Parse the .env file
-$Loader->parse();
+    $Loader->parse();
 // Send the parsed .env file to the $_ENV variable
-$Loader->toEnv();
+    $Loader->toEnv();
 
 // Instantiate PHP-DI ContainerBuilder
-$containerBuilder = new ContainerBuilder();
-if (!Env::isDebug()) { // Should be set to true in production
-    $containerBuilder->enableCompilation(__DIR__ . '/../cache');
-}
+    $containerBuilder = new ContainerBuilder();
+    if (!Env::isDebug()) { // Should be set to true in production
+        $containerBuilder->enableCompilation(__DIR__ . '/../cache');
+    }
 
 // Set up settings
-$settings = require __DIR__ . '/../app/settings.php';
-$settings($containerBuilder);
+    $settings = require __DIR__ . '/../app/settings.php';
+    $settings($containerBuilder);
 
 // Set up dependencies
-$dependencies = require __DIR__ . '/../app/dependencies.php';
-$dependencies($containerBuilder);
-
-// Set up repositories
-$repositories = require __DIR__ . '/../app/repositories.php';
-$repositories($containerBuilder);
+    $dependencies = require __DIR__ . '/../app/dependencies.php';
+    $dependencies($containerBuilder);
 
 // Build PHP-DI Container instance
-$container = $containerBuilder->build();
+    $container = $containerBuilder->build();
 
 // Instantiate the app
-AppFactory::setContainer($container);
-$app = AppFactory::create();
-$callableResolver = $app->getCallableResolver();
+    AppFactory::setContainer($container);
+    $app = AppFactory::create();
+    $callableResolver = $app->getCallableResolver();
 
 // Register middleware
-$middleware = require __DIR__ . '/../app/middleware.php';
-$middleware($app);
+    $middleware = require __DIR__ . '/../app/middleware.php';
+    $middleware($app);
 
 // Register routes
-$routes = require __DIR__ . '/../app/routes.php';
-$routes($app);
+    $routes = require __DIR__ . '/../app/routes.php';
+    $routes($app);
 
-/** @var bool $displayErrorDetails */
-$displayErrorDetails = $container->get('settings')['displayErrorDetails'];
+    /** @var bool $displayErrorDetails */
+    $displayErrorDetails = $container->get('settings')['displayErrorDetails'];
 
 // Create Request object from globals
-$serverRequestCreator = ServerRequestCreatorFactory::create();
-$request = $serverRequestCreator->createServerRequestFromGlobals();
+    $serverRequestCreator = ServerRequestCreatorFactory::create();
+    $request = $serverRequestCreator->createServerRequestFromGlobals();
 
 // Create Error Handler
-$responseFactory = $app->getResponseFactory();
-$errorHandler = new HttpErrorHandler($callableResolver, $responseFactory);
+    $responseFactory = $app->getResponseFactory();
+    $errorHandler = new HttpErrorHandler($callableResolver, $responseFactory);
 
 // Create Shutdown Handler
-$shutdownHandler = new ShutdownHandler($request, $errorHandler, $displayErrorDetails);
-register_shutdown_function($shutdownHandler);
+    $shutdownHandler = new ShutdownHandler($request, $errorHandler, $displayErrorDetails);
+    register_shutdown_function($shutdownHandler);
 
 // Add Routing Middleware
-$app->addRoutingMiddleware();
+    $app->addRoutingMiddleware();
 
 // Add Error Middleware
-$errorMiddleware = $app->addErrorMiddleware($displayErrorDetails, false, false);
-$errorMiddleware->setDefaultErrorHandler($errorHandler);
+    $errorMiddleware = $app->addErrorMiddleware($displayErrorDetails, false, false);
+    $errorMiddleware->setDefaultErrorHandler($errorHandler);
 
 // Run App & Emit Response
-$response = $app->handle($request);
-$responseEmitter = new ResponseEmitter();
-$responseEmitter->emit($response);
+    $response = $app->handle($request);
+    $responseEmitter = new ResponseEmitter();
+    $responseEmitter->emit($response);
+} catch (\Exception $e) {
+    echo "An error has happened: " . $e->getMessage() . PHP_EOL;
+}
