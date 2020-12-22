2a188b9c5c5ea6d08258cc75a53ef009f9352245
Angel X. Moreno <angelxmoreno@gmail.com>
2020-12-22 01:08:14 -0500

    added sys logger for heroku logs

--- config/dependencies.php
+++ config/dependencies.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 use App\Application\Middleware\SentryMiddleware;
 use App\Utils\CakephpLoader;
+use App\Utils\Settings;
 use DI\ContainerBuilder;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
@@ -17,16 +18,14 @@ return function (ContainerBuilder $containerBuilder) {
             return new SentryMiddleware($container->get('settings')['sentry']);
         },
         LoggerInterface::class => function (ContainerInterface $c) {
-            $settings = $c->get('settings');
-
-            $loggerSettings = $settings['logger'];
+            $loggerSettings = Settings::get($c, 'logger');
             $logger = new Logger($loggerSettings['name']);
 
             $processor = new UidProcessor();
             $logger->pushProcessor($processor);
 
-            $handler = new StreamHandler($loggerSettings['path'], $loggerSettings['level']);
-            $logger->pushHandler($handler);
+            $logger->pushHandler(new StreamHandler($loggerSettings['path'], $loggerSettings['level']));
+            $logger->pushHandler(new StreamHandler('php://stderr', Logger::WARNING));
 
             return $logger;
         },
