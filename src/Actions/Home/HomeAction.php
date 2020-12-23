460a57f3b5df36f0263b44618f7b30fc79dd2fa9
Angel X. Moreno <angelxmoreno@gmail.com>
2020-12-22 20:09:26 -0500

    Added cache config settings viewer

--- src/Actions/Home/HomeAction.php
+++ src/Actions/Home/HomeAction.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Actions\Home;
 
 use App\Model\Table\RequestLogsTable;
+use Cake\Cache\Cache;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Log\LoggerInterface;
 
@@ -36,7 +37,10 @@ class HomeAction extends \App\Actions\Action
         $connection = $this->requestLogsTable->getConnection();
         $driver = get_class($connection->getDriver());
         $isConnected = $connection->getDriver()->enabled();
-        $cache = get_class($connection->getCacher());
+        $cache = [
+            'engine'=>get_class($connection->getCacher()),
+            'config'=> Cache::getConfig('_cake_model_')
+        ];
         return $this->respondWithData([
             'database' => compact('isConnected', 'cache', 'driver')
         ]);
